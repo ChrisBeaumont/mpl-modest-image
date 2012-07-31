proper masking of NANs, but with a performance hit. Fix this if possible

modest_image/modest_image.py
@@ -3,7 +3,8 @@ rcParams = matplotlib.rcParams
 
 import matplotlib.image as mi
 import matplotlib.colors as mcolors
-
+import matplotlib.cbook as cbook
+import numpy as np
 
 class ModestImage(mi.AxesImage):
     """
@@ -32,9 +33,27 @@ class ModestImage(mi.AxesImage):
         super(ModestImage, self).__init__(*args, **kwargs)
 
     def set_data(self, A):
-        super(ModestImage, self).set_data(A)
+        """
+        Set the image array
+
+        ACCEPTS: numpy/PIL Image A
+        """
         self._full_res = A
-        self._sx, self._sy = None, None  # force redraw
+        self._A = A
+
+        if self._A.dtype != np.uint8 and not np.can_cast(self._A.dtype,
+                                                         np.float):
+            raise TypeError("Image data can not convert to float")
+
+        if (self._A.ndim not in (2, 3) or
+            (self._A.ndim == 3 and self._A.shape[-1] not in (3, 4))):
+            raise TypeError("Invalid dimensions for image data")
+
+        self._imcache =None
+        self._rgbacache = None
+        self._oldxslice = None
+        self._oldyslice = None
+        self._sx, self._sy = None, None
 
     def get_array(self):
         """Override to return the full-resolution array"""
@@ -59,12 +78,13 @@ class ModestImage(mi.AxesImage):
         sx = int(max(1, min((x1 - x0) / 5., dx / ext[0] / 1.2)))
 
         # have we already calculated what we need?
-        if sx == self._sx and sy == self._sy and \
+        if sx >= self._sx and sy >= self._sy and \
             x0 >= self._bounds[0] and x1 <= self._bounds[1] and \
             y0 >= self._bounds[2] and y1 <= self._bounds[3]:
             return
 
         self._A = self._full_res[y0:y1:sy, x0:x1:sx]
+        self._A = cbook.safe_masked_invalid(self._A)
         x1 = x0 + self._A.shape[1] * sx
         y1 = y0 + self._A.shape[0] * sy
 